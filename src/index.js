4e56851c9bfd0b10dd822f4a5745f68f51639b04
Nadzeya Vaskouskaya <nadiavaskovskaya@gmail.com>
2024-10-27 22:39:07 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const x = [];
+  const a = {};
+  const b = {};
+
+  bracketsConfig.forEach(([c, d]) => {
+    a[c] = d;
+    b[d] = c;
+  });
+
+  for (let i of str) {
+    if (a[i]) {
+      if (i === a[i] && x.length > 0 && x[x.length - 1] === i) {
+        x.pop();
+      } else {
+        x.push(i);
+      }
+    } else if (b[i]) {
+      if (x.length === 0 || x.pop() !== b[i]) {
+        return false;
+      }
+    }
+  }
+
+  return x.length === 0;
+};
